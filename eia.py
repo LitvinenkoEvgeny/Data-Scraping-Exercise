3c8ee015a2a45278c066d5702e07912b85afa82a
LitvinenkoEvgeny <evgenylitvinenkofrontend@gmail.com>
2015-11-20 20:58:26 +0300

    datapackage.json

--- eia.py
+++ eia.py
@@ -23,7 +23,7 @@ class Day(object):
     def __init__(self):
         super(Day, self).__init__()
         self.url = 'http://www.eia.gov/dnav/ng/hist/rngwhhdD.htm'
-        self.filename = 'day.csv'
+        self.filename = 'data/day.csv'
         self.site = requests.get(self.url)
         self.body = html.fromstring(self.site.content)
         self.head = ['Week of', 'Mon', 'Tue', 'Wed', 'Thu', 'Fri']
@@ -67,7 +67,7 @@ class Week(object):
     def __init__(self):
         super(Week, self).__init__()
         self.url = 'http://www.eia.gov/dnav/ng/hist/rngwhhdW.htm'
-        self.filename = 'week.csv'
+        self.filename = 'data/week.csv'
         self.site = requests.get(self.url)
         self.body = html.fromstring(self.site.content)
         self.head = ['Year-month', 'Week1 End Date', 'Week1 Value',
@@ -117,7 +117,7 @@ class Month(object):
     def __init__(self):
         super(Month, self).__init__()
         self.url = 'http://www.eia.gov/dnav/ng/hist/rngwhhdM.htm'
-        self.filename = 'month.csv'
+        self.filename = 'data/month.csv'
         self.site = requests.get(self.url)
         self.body = html.fromstring(self.site.content)
         self.head = ['Year', 'Jan', 'Feb',
@@ -163,6 +163,56 @@ class Month(object):
         self.parse_data()
 
 
+class Annual(object):
+    """parse data from day page"""
+
+    def __init__(self):
+        super(Annual, self).__init__()
+        self.url = 'http://www.eia.gov/dnav/ng/hist/rngwhhdA.htm'
+        self.filename = 'data/annual.csv'
+        self.site = requests.get(self.url)
+        self.body = html.fromstring(self.site.content)
+        self.head = ['Decade', 'Year-0', 'Year-2', 'Year-3', 'Year-4', 'Year-5',
+                     'Year-6', 'Year-7', 'Year-8', 'Year-9']
+
+    def parse_data(self):
+        trs = self.body.xpath('//table[@cellpadding="2"]/tr')[1:]
+        all_data = []
+        for tr in trs:
+            decade = self.clean_empty(tr.xpath('.//td[1]/text()'))
+            y0 = self.clean_empty(tr.xpath('./td[2]/text()'))
+            y1 = self.clean_empty(tr.xpath('./td[3]/text()'))
+            y2 = self.clean_empty(tr.xpath('./td[4]/text()'))
+            y3 = self.clean_empty(tr.xpath('./td[5]/text()'))
+            y4 = self.clean_empty(tr.xpath('./td[6]/text()'))
+            y5 = self.clean_empty(tr.xpath('./td[7]/text()'))
+            y6 = self.clean_empty(tr.xpath('./td[8]/text()'))
+            y7 = self.clean_empty(tr.xpath('./td[9]/text()'))
+            y8 = self.clean_empty(tr.xpath('./td[10]/text()'))
+            y9 = self.clean_empty(tr.xpath('./td[11]/text()'))
+
+            data = [decade, y0, y1, y2, y3, y4, y5, y6, y7, y8, y9]
+            data = [clean_date(item) for item in data]
+            all_data.append(data)
+
+        all_data = [array for array in all_data if array[0]]
+        all_data.insert(0, self.head)
+        write_to_csv(self.filename, all_data)
+
+    def clean_empty(self, elem):
+        if len(elem) > 0:
+            return elem[0]
+
+    def clean_date(self, item):
+        try:
+            return re.sub(r'\xa0', u'', item)
+        except TypeError:
+            pass
+
+    def main(self):
+        self.parse_data()
+
+
 if __name__ == '__main__':
     day = Day()
     day.main()
@@ -170,3 +220,5 @@ if __name__ == '__main__':
     week.main()
     month = Month()
     month.main()
+    annual = Annual()
+    annual.main()
